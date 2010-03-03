848e9475b4b3a81a43f1c8593510e28240cdbbc1
Simon Chiang <simon.chiang@pinnacol.com>
2010-03-03 09:09:41 -0700

    made constants specify the platform-specific libsuffix

--- lib/clipr/constants.rb
+++ lib/clipr/constants.rb
@@ -1,6 +1,21 @@
+require 'rbconfig'
+
 module Clipr
+  
+  # Adapted from FFI::Platform
+  LIBSUFFIX = case Config::CONFIG['host_os'].downcase
+  when /linux|freebsd|openbsd|sunos|solaris/
+    'so'
+  when /darwin/
+    'bundle'
+  when /win|mingw/
+    'dll'
+  else
+    'so'
+  end
+  
   ROOT = File.expand_path("#{File.dirname(__FILE__)}/../..")
-  DYLIB = File.join(ROOT, "src/clips.bundle")
+  DYLIB = File.join(ROOT, "src/clips.#{LIBSUFFIX}")
   
   # From constant.h
   GLOBAL_SAVE  = 0
